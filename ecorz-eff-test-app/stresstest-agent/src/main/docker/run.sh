481dcb83dc8fe33120f1514a49cbce6a9f05bccd
Florian Held <florian.held@uni-ulm.de>
2020-01-10 13:38:05 +0100

    build jar with spriongboot maven plugin

--- ecorz-eff-test-app/stresstest-agent/src/main/docker/run.sh
+++ ecorz-eff-test-app/stresstest-agent/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 
 echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
 echo "********************************************************"
-java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/stresstest_agent/@project.build.finalName@-jar-with-dependencies.jar
+javaw -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/stresstest_agent/@project.build.finalName@-jar-with-dependencies.jar
