d0086f4c48c3190a5852618140e6d9446d133a51
Florian Held <florian.held@uni-ulm.de>
2020-01-10 13:01:23 +0100

    set corrected dependency-jar name in entrpoint scripts

--- ecorz-eff-test-app/confsvr/src/main/docker/run.sh
+++ ecorz-eff-test-app/confsvr/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting Configuration Server"
 echo "********************************************************"
-java -jar /usr/local/configserver/@project.build.finalName@.jar
+java -jar /usr/local/configserver/@project.build.finalName@-jar-with-dependencies.jar
