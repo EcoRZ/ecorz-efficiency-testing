01ba36e84b0a393cfa06625dfcfc9822b1eba21c
Florian Held <florian.held@uni-ulm.de>
2020-01-12 14:55:10 +0100

    set logging via output of java start command

--- ecorz-eff-test-app/stresstest-agent/src/main/docker/run.sh
+++ ecorz-eff-test-app/stresstest-agent/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 
 echo "Starting StressTest Agent with Configuration Service :  $CONFIGSERVER_URI";
 echo "********************************************************"
-java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -Dspring.datasource.url="$RESULTS_DB_URI" -jar /usr/local/stresstest_agent/@project.build.finalName@.jar
+java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -Dspring.datasource.url="$RESULTS_DB_URI" -jar /usr/local/stresstest_agent/@project.build.finalName@.jar > spring.log 2>&1
