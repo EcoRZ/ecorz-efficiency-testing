7f766ef1499ace78285eb2eac107fb051823c56f
Florian Held <florian.held@uni-ulm.de>
2020-01-17 14:47:42 +0100

    init set correct directories for jmeter-dump in stresstestagent container

--- ecorz-eff-test-app/stresstest-agent/src/main/docker/run.sh
+++ ecorz-eff-test-app/stresstest-agent/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 
 echo "Starting StressTest Agent with Configuration Service :  $CONFIGSERVER_URI";
 echo "********************************************************"
-java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -Dspring.datasource.url="$RESULTS_DB_URI" -Decorz.jmeter.home=${JMETER_HOME} -Decorz.jmeter.home.bin=${JMETER_BIN_DIR} -Decorz.jmeter.fs.base.directory=${JMETER_FS_BASE} -Decorz.lb.ip=${LB_IP} -jar /usr/local/stresstest_agent/@project.build.finalName@.jar > spring.log 2>&1
+java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -Dspring.datasource.url="$RESULTS_DB_URI" -Decorz.jmeter.home=${JMETER_HOME} -Decorz.jmeter.home.bin=${JMETER_BIN_DIR} -Decorz.jmeter.fs.base.directory=${JMETER_FS_BASE} -Decorz.lb.ip=${LB_IP} -Decorz.jmeter.engine.cp=${JMETER_CP} -jar /usr/local/stresstest_agent/@project.build.finalName@.jar > spring.log 2>&1
