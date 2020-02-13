42d1589789d13ecd81d858a857f748017c54d33f
Florian Held <florian.held@uni-ulm.de>
2020-02-13 09:13:47 +0100

    set resutsdumpfolder in application.properties

--- ecorz-eff-test-app/stresstest-agent/src/main/docker/run.sh
+++ ecorz-eff-test-app/stresstest-agent/src/main/docker/run.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-
-arrCP=(${JMETER_CP//:/ })
-cp_dump_folder=${arrCP[1]}
+#!/bin/sh
 
 echo "Starting StressTest Agent with Configuration Service :  $CONFIGSERVER_URI";
 echo "********************************************************"
 java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -Dspring.datasource.url="$RESULTS_DB_URI" \
 -Decorz.jmeter.home=${JMETER_HOME} -Decorz.jmeter.home.bin=${JMETER_BIN_DIR} -Decorz.jmeter.fs.base.directory=${JMETER_FS_BASE} \
 -Decorz.lb.ip=${LB_IP} -Decorz.jmeter.engine.cp=${JMETER_CP} -Decorz.prometheus.user=${PROM_USER} -Decorz.prometheus.password=${PROM_PW} \
--Decorz.prometheus.rest.end=${PROM_REST_END} -Decorz.results.resultsdumpfolder=${cp_dump_folder} -jar ${JAR_DIR}/@project.build.finalName@.jar # > spring.log 2>&1
+-Decorz.prometheus.rest.end=${PROM_REST_END} -jar ${JAR_DIR}/@project.build.finalName@.jar # > spring.log 2>&1
