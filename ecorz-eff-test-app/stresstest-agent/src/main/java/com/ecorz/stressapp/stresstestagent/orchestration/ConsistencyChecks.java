649ba8b6ee99decddf752359f0c9c08f98c3002b
Florian Held <florian.held@uni-ulm.de>
2020-02-13 09:50:00 +0100

    debug cp

--- ecorz-eff-test-app/stresstest-agent/src/main/java/com/ecorz/stressapp/stresstestagent/orchestration/ConsistencyChecks.java
+++ ecorz-eff-test-app/stresstest-agent/src/main/java/com/ecorz/stressapp/stresstestagent/orchestration/ConsistencyChecks.java
@@ -1,15 +1,20 @@
 package com.ecorz.stressapp.stresstestagent.orchestration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ConsistencyChecks {
 
   private static final String cPDel = ":";
   private static final int cPLength = 2;
+  private final static Logger LOGGER = LoggerFactory.getLogger(ConsistencyChecks.class);
 
   private ConsistencyChecks() {
   }
 
   public static boolean dumpFolderIsConsistent(String jMeterClassPath, String configDumpFolder) {
     String[] cPSplitted = jMeterClassPath.split(cPDel);
+    LOGGER.error("stuff: " + cPSplitted[cPLength-1] + ", " + configDumpFolder);
 
     if(cPSplitted.length != cPLength) {
       return false;
