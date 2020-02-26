0ff55c4e0b33697057cc29dc9811ad061019792c
Florian Held <florian.held@uni-ulm.de>
2020-02-26 18:00:12 +0100

    use consistency check again

--- ecorz-eff-test-app/stresstest-agent/src/main/java/com/ecorz/stressapp/stresstestagent/orchestration/Orchestrator.java
+++ ecorz-eff-test-app/stresstest-agent/src/main/java/com/ecorz/stressapp/stresstestagent/orchestration/Orchestrator.java
@@ -60,12 +60,12 @@ public class Orchestrator {
   public UUID startRun(UUID runUuid) throws RunException {
     UUID orchestrationUuid = UUID.randomUUID();
 
-    /*if(!ConsistencyChecks.dumpFolderIsConsistent(
+    if(!ConsistencyChecks.dumpFolderIsConsistent(
         jmeterService.getJMeterCP(), resultService.getResultsDumpFolder()
     )) {
       throw new RunException(String.format("JMeter cp: %s and dumpFolder: %s are not consistent",
          jmeterService.getJMeterCP(), resultService.getResultsDumpFolder()));
-    }*/
+    }
 
     EvalDates evalDates = startRunJMeter(runUuid, orchestrationUuid);
     startRunPrometheus(runUuid, orchestrationUuid, evalDates);
